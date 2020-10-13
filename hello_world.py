0fa34e988ee9602019041c8eaa05cf97e0f8227a
Sarit Kumar Das <quantumalex008@gmail.com>
2020-10-13 10:40:28 +0530

    Add dog translation for hello_world

--- hello_world.py
+++ hello_world.py
@@ -1,5 +1,11 @@
+def dog_translation():
+    print('Dog Translation : woof wooof')
+
+def main():
+    welcome()
+
 def welcome():
     print('Hello World')
 
 if __name__ == '__main__':
-    welcome();
+    main()
